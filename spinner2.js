efc95d4483a27802ea95a781508b80b28f9aa04c
aal-brown <adlai.brown@gmail.com>
2020-01-14 21:21:58 +0000

    Spinner1 refactored and saved as spinner2.

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+const stringArray = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
+let delay = 100;
+for (const string of stringArray) {
+  setTimeout(() => {
+    process.stdout.write(string);
+  }, delay);
+  delay += 200;
+}
